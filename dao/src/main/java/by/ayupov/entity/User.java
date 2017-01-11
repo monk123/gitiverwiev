c3fe48062b38f9a778516ed74e279a7d4a61f5bb
Vadim Ayupov <ayub94.mogilev@gmail.com>
2017-01-11 17:05:04 +0300

    add new dependency

--- dao/src/main/java/by/ayupov/entity/User.java
+++ dao/src/main/java/by/ayupov/entity/User.java
@@ -1,6 +1,6 @@
-package by.ayupov;
+package by.ayupov.entity;
 
-public class App {
+public class User {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
